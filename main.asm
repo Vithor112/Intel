e3e23e2211fb79a6ee4d1a382997d21a04a84039
Vithor <vithorpileco@gmail.com>
2022-04-20 02:11:34 -0300

    Implements intToString, findChar, findEndString and scanRep

--- main.asm
+++ main.asm
@@ -1,6 +1,11 @@
 .model small
 .stack
 .data
+temp dw ?
+pointerChar dw ?
+FinalTen db 10
+pointerString dw ?
+StringTemp db 20 dup(0)
 Texto1  db "Insira o nome do arquivo:",CR,LF,endString
 NomeArquivo db 61 dup(0)
 msgError db " Houve um erro ", endString
@@ -15,40 +20,114 @@ msgError db " Houve um erro ", endString
 	call open_f
 .exit
 
+;================================================================================================================================================
+; IO FUNCS  IO FUNCS  IO FUNCS  IO FUNCS  IO FUNCS  IO FUNCS  IO FUNCS  IO FUNCS  IO FUNCS  IO FUNCS  IO FUNCS  IO FUNCS  IO FUNCS  IO FUNCS  
+;================================================================================================================================================
+
+LIMIT_SCAN equ 60
 ; Scanea caracteres do teclado, até um máximo de 60 e os armazena na string apontada por bx
 	scanf proc near
-		mov cx,60
-		mov ah,1
-input:		int 21h
-		cmp al,13
-		jz end_input
-		mov [bx],al
-		inc bx;
-		loop input
-end_input:	mov [bx], endString
+		mov cx,LIMIT_SCAN                 ;; Limite de Caracteres
+		mov ah,1			  ;; função 1
+input:		int 21h				  ;; Chamada função DOS
+		cmp al,CR			  ;; Verifica se recebeu um enter como entrada
+		jz end_input			  ;; Se sim Termina de receber input
+		mov [bx],al			  ;; Se não coloca o char na string
+		inc bx;				  ;; E Incrementa seu ponteiro
+		loop input			 
+end_input:	mov [bx], endString	 	  ;; Coloca o terminador de String no Final da string
 		ret
 	scanf endp
 	
 ; Printa a string que está no ponteiro dx, a string precisa terminar com $
 	printf_s proc near
-		mov ah,9
+		mov ah,9		
 		int 21h
 		ret
 	printf_s endp
 
+;================================================================================================================================================
+; CONVERT FUNCS CONVERT FUNCS CONVERT FUNCS CONVERT FUNCS CONVERT FUNCS CONVERT FUNCS CONVERT FUNCS CONVERT FUNCS CONVERT FUNCS CONVERT FUNCS 
+;================================================================================================================================================
+
+;; Converte o inteiro em ax para uma string armazenada em dx
+	intToString proc near
+		push dx				;; Salva dx
+		push bp				;; Salva bp
+		push bx				;; Salva bx
+		push cx 			;; Salva cx
+		push ax				;; Salva ax
+		mov bp,dx			
+		mov cx,5		
+		mov bx,10000		;; Pois 16 bits suportam no máximo um inteiro igual a 65535
+		mov dx,0			;; Limpa dx, pois DX:AX/BX
+toStringInt:	
+		div bx				;; Divisão de 16 bits/16 bits
+		add ax, "0"			;; Converte o algarismo em char
+		mov [bp], ax		;; Coloca na string 
+		inc bp				;; Incrementa ponteiro
+		mov ax,bx			;; ax = bx
+		div FinalTen		;; ax /= 10 ( resto vai ser igual a 0 )
+		mov bx,ax			;; bx = ax
+		mov ax,dx			;; Transfere o resto para o dividendo 
+		mov dx,0			;; Limpa o resto
+		loop toStringInt
+		mov [bp], endString ;; Coloca o terminador na Strings
+		pop ax			;; Retorna ax
+		pop cx			;; Retorna cx
+		pop bx			;; Retorna bx
+		pop bp			;; Retorna bp
+		pop dx			;; Retorna dx
+		ret
+	intToString endp
+
+;================================================================================================================================================
+; STRING FUNCS STRING FUNCS STRING FUNCS STRING FUNCS STRING FUNCS STRING FUNCS STRING FUNCS STRING FUNCS STRING FUNCS STRING FUNCS STRING FUNCS 
+;================================================================================================================================================
+
 ; Scannea String em dx pela primeira ocorrência do char em ch e o troca por cl
 ; o Char em ch precisa estar na string! 
-        scanRep proc near
-                push bp
-                mov bp,dx
-                dec bp
-keepLooking:    inc bp  
-                cmp [bp],ch
-                jne keepLooking
-                mov [bp],cl
-                pop bp
+	scanRep proc near
+			push bp
+			call findChar
+			mov [bp],cl
+			pop bp
+			ret
+	scanRep endp
+
+		
+;; Encontra a primeira ocorrÊncia do char em ch na string em dx e retorna um ponteiro para este char em bp
+	findChar  proc near
+                mov bp,dx			;; bp = dx
+                dec bp				;; bp-- ( ajustar para o primeiro loop )
+	lookChar:   inc bp				;; inc++
+                cmp [bp],ch			
+                jne lookChar		;; Se não forem iguais continua procurando
                 ret
-        scanRep endp
+	findChar	  endp 
+
+
+;; Retorna um ponteiro para o final da String ( deve terminar com $ )
+	FindendString proc near
+		mov bp, pointerChar
+		mov ch, endString	
+		call findChar
+		ret
+	FindendString endp
+
+
+
+
+	getErrorMessage proc near
+		call FindendString 
+		mov [bp], SPACE
+		inc bp
+		lea dx, StringTemp 
+		call intToString 
+		;; TODO
+		ret
+	getErrorMessage endp
+
 
 ; Abre Arquivo cujo nome está na string apontada por dx, abre em modo de leitura
 	open_f proc near
@@ -60,9 +139,10 @@ keepLooking:    inc bp
 		int 21h
 		jnc no_error
 		lea dx, msgError
+		call getErrorMessage
 		call printf_s
 no_error:	mov ch, 00H
-		mov cl, eddndString
+		mov cl, endString
 		call scanRep
 		ret
 	open_f endp
@@ -72,6 +152,6 @@ CR		equ	 13
 endString	equ	 36
 null		equ 	00h
 LF 		equ	 10
-
+SPACE		equ 	20h
 end
 		
